fe098157c43bf4f6ff57a80def96b2a1dae1d136
Daniel Choi <daniel.won.choi@gmail.com>
2016-07-03 00:03:37 -0700

    jump done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,27 @@ public class PlayerMovement : MonoBehaviour {
 
     public float speed;
     public float jumpForce;
+    public bool isGround;
+    public LayerMask whatIsGround;
 
     private Rigidbody2D playerRigidBody;
+    private Collider2D myCollider;
 
 	// Use this for initialization
 	void Start () {
         playerRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        isGround = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
         playerRigidBody.velocity = new Vector2(speed, playerRigidBody.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space)){
-            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpForce);
+            if (isGround) {
+                playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpForce);
+            }
         }
 	}
 }
