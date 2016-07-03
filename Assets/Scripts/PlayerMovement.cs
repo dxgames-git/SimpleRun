f0dc78cf46a0e35b8c007fee59601d17825cf56e
Daniel Choi <daniel.won.choi@gmail.com>
2016-07-02 23:40:02 -0700

    velocity done

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+
+    private Rigidbody2D playerRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerRigidBody.velocity = new Vector2(speed, playerRigidBody.velocity.y);
+	}
+}
