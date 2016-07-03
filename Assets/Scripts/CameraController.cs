fb3f34e9d471e8342ab0cfa4db4afce33636365a
Heesoo Home <Heesoo@Heesoo-PC>
2016-07-03 00:15:49 -0700

    Camera Motion Implemented

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject thePlayer;
+    private float level;
+
+	// Use this for initialization
+	void Start () {
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
+        level = thePlayer.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(thePlayer.transform.position.x + 3, level, transform.position.z);
+	}
+}
