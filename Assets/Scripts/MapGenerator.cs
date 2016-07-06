d0cd9cb18676b6f69ef0c23fb99ee69ccfee0771
Daniel Choi <daniel.won.choi@gmail.com>
2016-07-06 16:56:18 -0700

    distance

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -36,6 +36,9 @@ public class MapGenerator: MonoBehaviour {
             transform.position = new Vector3(transform.position.x + platformWidth * 1.280459f + distanceBetween, transform.position.y, transform.position.z);
 
             Instantiate(thePlatform, transform.position, transform.rotation);
+
+            //generating different angles
+
             generate(new Vector3(transform.position.x, transform.position.y, transform.position.z));
         }
 	}
@@ -48,10 +51,11 @@ public class MapGenerator: MonoBehaviour {
         {
             num2 = (int)(3 * Random.value);
         }
-        Vector3 distance = new Vector3(position.x, position.y - 5, -1);
-        Instantiate(obstacleChooser[num1], distance, transform.rotation);
+        float randomRotation = 90 * Random.value;
+        Vector3 distance = new Vector3(position.x + 20 * Random.value, position.y - 5, -1);
+        Instantiate(obstacleChooser[num1], distance, Quaternion.Euler(90 * Random.value, 90 * Random.value, 0));
         distance = new Vector3(distance.x + minDistanceBetweenObstacles, distance.y, distance.z);
-        Instantiate(obstacleChooser[num2], distance, transform.rotation);
+        Instantiate(obstacleChooser[num2], distance, Quaternion.Euler(90 * Random.value, 90 * Random.value, 0));
     }
 
 }
