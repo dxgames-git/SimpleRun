34299bd4749ce9a3ee0e61f8e47207875957d7e5
Heesoo Home <Heesoo@Heesoo-PC>
2016-07-05 22:46:45 -0700

    Regular Obstacle Spawning

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -3,15 +3,19 @@ using System.Collections;
 
 public class MapGenerator: MonoBehaviour {
 
+    //Map Generation Fields
     public GameObject thePlatform;
     public Transform generationPoint;
     public float distanceBetween;
 
     private float platformWidth;
-    private ObstaclesGenerator theObstaclesGenerator;
 
-	// Use this for initialization
-	void Start () {
+    //Obstacle Generation Fields
+    public GameObject obstacle;
+    public float minDistanceBetweenObstacles;
+
+    // Use this for initialization
+    void Start () {
         platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
     }
 	
@@ -23,7 +27,21 @@ public class MapGenerator: MonoBehaviour {
             transform.position = new Vector3(transform.position.x + platformWidth * 1.280459f + distanceBetween, transform.position.y, transform.position.z);
 
             Instantiate(thePlatform, transform.position, transform.rotation);
-            theObstaclesGenerator.generate(new Vector3(transform.position.x, transform.position.y, transform.position.z));
+            generate(new Vector3(transform.position.x, transform.position.y, transform.position.z));
         }
 	}
+
+    public void generate(Vector3 position)
+    {
+        float numObstacles;
+        numObstacles = (float)(3f * Random.value + 1f);
+        Vector3 distance = position;
+
+        /*for (int i = 0; i < numObstacles; i++)
+        {*/
+            Instantiate(obstacle, distance, transform.rotation);
+            distance = new Vector3(distance.x + minDistanceBetweenObstacles, distance.y, distance.z);
+        //}
+    }
+
 }
