a715ccbf1dba62b09a510ab29321e8c0d0ffcd90
Heesoo Home <Heesoo@Heesoo-PC>
2016-07-07 22:46:50 -0700

    fixed eraser outofbounds

--- Assets/Scripts/EraserMovement.cs
+++ Assets/Scripts/EraserMovement.cs
@@ -19,6 +19,10 @@ public class EraserMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         timeCounter += Time.deltaTime;
+        if (timeCounter >= 2f * Mathf.PI)
+        {
+            timeCounter -= 2f * Mathf.PI;
+        }
         transform.position = new Vector3(thePlayer.transform.position.x - distanceToPlayer, Mathf.Sin(timeCounter * eraserSpeed) * eraserRange * Time.deltaTime, thePlayer.transform.position.z);
 	}
 }
