85e0776ab39327a2501f30b6dcf3290f51630b60
Heesoo Home <Heesoo@Heesoo-PC>
2016-07-07 22:36:39 -0700

    better script

--- Assets/Scripts/EraserMovement.cs
+++ Assets/Scripts/EraserMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class EraserMovement : MonoBehaviour {
 
-    public float speed;
+    /*public float speed;
     public float upDownSpeed;
     public float distanceFromCenter;
     public GameObject thePlayer;
@@ -11,25 +11,29 @@ public class EraserMovement : MonoBehaviour {
     private Rigidbody2D eraserRigidBody;
     private Collider2D eraserCollider;
     private Vector3 eraserStartPoint;
-    private bool upOrDown;
+    private bool upOrDown;*/
+    public float distanceToPlayer;
+    private GameObject thePlayer;
 
     // Use this for initialization
     void Start () {
-        eraserRigidBody = GetComponent<Rigidbody2D>();
+        /*eraserRigidBody = GetComponent<Rigidbody2D>();
         eraserCollider = GetComponent<PolygonCollider2D>();
         eraserStartPoint = transform.position;
-        eraserRigidBody.velocity = new Vector2(speed, upDownSpeed);
+        eraserRigidBody.velocity = new Vector2(speed, upDownSpeed);*/
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (transform.position.y > eraserStartPoint.y + distanceFromCenter && upOrDown) {
+        /*if (transform.position.y > eraserStartPoint.y + distanceFromCenter && upOrDown) {
             eraserRigidBody.velocity = new Vector2(speed, -1 * upDownSpeed);
             upOrDown = false;
         }
         if (transform.position.y < eraserStartPoint.y - distanceFromCenter && upOrDown == false) {
             eraserRigidBody.velocity = new Vector2(speed, -1 * upDownSpeed);
             upOrDown = true;
-        }
+        }*/
+        transform.position = new Vector3(thePlayer.transform.position.x - distanceToPlayer, Mathf.Sin(Time.deltaTime), thePlayer.transform.position.z);
 	}
 }
