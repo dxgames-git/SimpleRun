cd5fa6d72fdb797da4a40d02feff40a838d4f4a1
Daniel Choi <daniel.won.choi@gmail.com>
2016-07-07 22:30:38 -0700

    eraser movement

---
+++ Assets/Scripts/EraserMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EraserMovement : MonoBehaviour {
+
+    public float speed;
+    public float upDownSpeed;
+    public float distanceFromCenter;
+    public GameObject thePlayer;
+    
+    private Rigidbody2D eraserRigidBody;
+    private Collider2D eraserCollider;
+    private Vector3 eraserStartPoint;
+    private bool upOrDown;
+
+    // Use this for initialization
+    void Start () {
+        eraserRigidBody = GetComponent<Rigidbody2D>();
+        eraserCollider = GetComponent<PolygonCollider2D>();
+        eraserStartPoint = transform.position;
+        eraserRigidBody.velocity = new Vector2(speed, upDownSpeed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y > eraserStartPoint.y + distanceFromCenter && upOrDown) {
+            eraserRigidBody.velocity = new Vector2(speed, -1 * upDownSpeed);
+            upOrDown = false;
+        }
+        if (transform.position.y < eraserStartPoint.y - distanceFromCenter && upOrDown == false) {
+            eraserRigidBody.velocity = new Vector2(speed, -1 * upDownSpeed);
+            upOrDown = true;
+        }
+	}
+}
