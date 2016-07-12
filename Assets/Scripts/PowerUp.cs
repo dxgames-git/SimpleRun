29ae78f6c6eb144a0f5204187b0a1ae4b921afce
Daniel Choi <daniel.won.choi@gmail.com>
2016-07-12 14:36:08 -0700

    speed up duration

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -3,20 +3,33 @@ using System.Collections;
 
 public class PowerUp : MonoBehaviour {
 
+    private float duration;
+
     private PlayerMovement player;
+    private EraserMovement eraser;
+    private bool didSpeedUp;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        eraser = GameObject.FindGameObjectWithTag("Eraser").GetComponent<EraserMovement>();
+        didSpeedUp = false;
+        duration = 10f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (didSpeedUp && Time.time > duration) {
+            player.speed = 6;
+            eraser.distanceToPlayer = 6;
+        }
 	
 	}
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
             player.speed = 10;
+            eraser.distanceToPlayer = 9;
+            didSpeedUp = true;
         }
     }
 }
